935ac301666dfbd2ec4bcff96bf3e2c9a1c887a0
blakewallan <blakewallan@gmail.com>
2016-01-12 21:51:45 -0800

    Scrape is working properly and converting addresses to longitude and latituide

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,15 @@
+require 'scraping_helper'
+
 class MainController < ApplicationController
 
   before_action :current_user
 
+
+
   def index
+
+    @data = ScrapingHelper.scrape
+    @results = ScrapingHelper.set_everything @data
 
   end
 
