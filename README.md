2c811e8f9c3584642d50fd7bae2476bf2b6314ed
John Stepan <jstep84@gmail.com>
2016-01-13 21:16:43 -0800

    edit README.md

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1>Synopsis</h1>
+<p>This is a synopsis about this project</p><b>
+
+<h1>Code Example</h1>
+<p>This is a code sample from this project</p><b>
+
+<h1>Motivation</h1>
+<p>This is why we created this project</p><b>
+
+<h1>Installation</h1>
+<p>This explains (with code examples) how the project can be accessed</p><b>
+
+<h1>Tests</h1>
+<p>n/a</p><b>
+
+<h1>Contributors</h1>
+<p>Here's how you can support the project</p><b>
+
+<h1>License</h1>
+<p>describing any licenses used in the project</p>
+
+
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,8 +9,8 @@ class MainController < ApplicationController
 
   def index
 
-    # @data = ScrapingHelper.scrape "http://www.resourcehouse.com/WIN211/results.aspx?SearchID=0FEBE320-EE8A-4770-AAC0-B9C27A8B8FD5"
-    # @results = ScrapingHelper.set_everything @data
+      # @data = ScrapingHelper.scrape "http://www.resourcehouse.com/WIN211/results.aspx?SearchID=0FEBE320-EE8A-4770-AAC0-B9C27A8B8FD5"
+      # @results = ScrapingHelper.set_everything @data
     #
     # @data = ScrapingHelper.scrape
     # @results = ScrapingHelper.set_everything @data
@@ -19,9 +19,10 @@ class MainController < ApplicationController
 
     #THIS SHIT POPULATES THE DB
 
-    # @results.each do |i|
-    #   @meals = Meal.create(i)
-    # end
+      # @results.each do |i|
+      #   @meals = Meal.create(i)
+      #   @shelters = Shelter.create(i)
+      # end
 
   	# @disable_nav = true
   end
