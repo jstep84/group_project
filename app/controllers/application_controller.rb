4489a267b300458eb52da76e779409e0dc376a55
John Stepan <jstep84@gmail.com>
2016-01-14 11:21:49 -0800

    add 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,8 @@ class ApplicationController < ActionController::Base
     @current_user = User.find_by_id(session[:user_id])
   end
 
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
+
 end
