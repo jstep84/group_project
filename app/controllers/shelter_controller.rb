76df6791d9d6d6814e9e85e196f91035f003b39f
blakewallan <blakewallan@gmail.com>
2016-01-15 11:01:29 -0800

    fixed user location stuff so the coords are available through cookies

--- app/controllers/shelter_controller.rb
+++ app/controllers/shelter_controller.rb
@@ -8,22 +8,15 @@ class ShelterController < ApplicationController
 
   def show
     @shelter = Shelter.all
-    if params[:lat2]
-      user_lat = params[:lat2]
-      user_long = params[:long2]
-    else
-      user_lat = params[:lat3]
-      user_long = params[:long3]
-    end
 
-    puts user_long
+    user_lat = cookies[:latitude]
+    user_long = cookies[:longitude]
+
     @shelter_results = []
 
     @shelter.each do |i|
       @shelter_results.push(distance: DistanceHelper.haversine(user_lat.to_f, user_long.to_f, i.latitude, i.longitude), result: i)
     end
-
-    puts @shelter_results[3][:distance]
 
     @shelter_results.sort_by! {|e| e[:distance]  }
 
